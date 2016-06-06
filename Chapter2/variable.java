2916c901402cd0d0d467cd4ee2202900eabf5a21
ArunRamachandran <arunkramachandran92@gmail.com>
2016-06-07 00:06:03 +0530

    Added Changes

---
+++ Chapter2/variable.java
@@ -0,0 +1,14 @@
+class Variable {
+	
+	public static void main(String args[]) {
+		String bob;
+		bob = "My name is bob !";
+		System.out.print(bob);
+
+		// Declaring an integer variable 
+		int a,b;
+		a = 10; b = 20;
+		System.out.print(a);
+		System.out.print(b);
+	}
+}
